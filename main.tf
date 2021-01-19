312f39ac1f057a05da29dbef25159604b11ff226
root <theerapong.k@jasmine.com>
2021-01-19 02:58:55 +0000

    Tue Jan 19 2021 - Committed via Codeherent

--- main.tf
+++ main.tf
@@ -1,31 +1,30 @@
-variable do_token{}
+variable do_token {}
 
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "2.3.0"
     }
   }
 }
 
-
 provider "digitalocean" {
   token = var.do_token
 }
 
-
-
 data "digitalocean_ssh_key" "work" {
-    name = "PublicKeyFromMac"
+  name = "PublicKeyFromMac"
 }
 
 resource "digitalocean_droplet" "web" {
-  image = "ubuntu-18-04-x64"
-  name  = "my new vm"
+  image  = "ubuntu-18-04-x64"
+  name   = "my new vm"
   region = "sgp1"
-  size = "s-1vcpu-1gb"
-  ssh_keys = [ data.digitalocean_ssh_key.work.id ]
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.work.id,
+  ]
 }
 
 output "server_ip" {
